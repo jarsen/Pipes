499cf5a57d79c0fd7759f7aadb50d62e0de3668a
jarsen <jason.larsen@gmail.com>
2015-04-28 09:54:36 -0600

    Initial Commit

---
+++ Pipes/Pipes.h
@@ -0,0 +1,19 @@
+//
+//  Pipes.h
+//  Pipes
+//
+//  Created by Jason Larsen on 4/28/15.
+//  Copyright (c) 2015 Jason Larsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pipes.
+FOUNDATION_EXPORT double PipesVersionNumber;
+
+//! Project version string for Pipes.
+FOUNDATION_EXPORT const unsigned char PipesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pipes/PublicHeader.h>
+
+
