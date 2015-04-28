a59c2f22ae3a3bcb1951c50960a0f92a8676b218
jarsen <jason.larsen@gmail.com>
2015-04-28 13:37:03 -0600

    add backward pipe

---
+++ Pipes/BackwardPipe.swift
@@ -0,0 +1,11 @@
+//
+//  Created by Jason Larsen on 4/27/15.
+//
+
+import Foundation
+
+infix operator <| { precedence 100 associativity right }
+
+public func <|<A,Z>(lhs: A->Z, rhs: A) -> Z {
+    return lhs(rhs)
+}
---
+++ PipesTests/BackwardPipeTests.swift
@@ -0,0 +1,25 @@
+//
+//  Created by Jason Larsen on 4/27/15.
+//
+
+import XCTest
+import Pipes
+
+class BackwardPipeTests : XCTestCase {
+    func testPipeOperatorWorks() {
+        let increment: Int->Int = { x in x + 1 }
+        let value = increment <| 4
+        expect(value) == 5
+    }
+    
+    func testOrderOfOperations() {
+        let increment: Int->Int = { x in x + 1 }
+        let double: Int->Int = {x in x*2}
+        
+        var value = double <| 2 + 4
+        expect(value) == 12
+
+        value = double <| increment <| 2
+        expect(value) == 6
+    }
+}
